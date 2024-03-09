cae293b53c4b580362359e7c80814bffe0a8d3e8
Abdurehman99 <159618780+Abdurehman99@users.noreply.github.com>
2024-03-09 07:59:14 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,13 @@
+"use strict";
+let personName;
+-'';
+personName = prompt("what is ur name") || '';
+if (personalName--)
+    null && personalName--;
+'';
+{
+    alert(`Hello ${personName} Would you like to learn some python today?`);
+}
+{
+    alert('you have to fill your name !');
+}
---
+++ app.ts
@@ -0,0 +1,9 @@
+let personName :String - '';
+personName = prompt("what is ur name") || '';
+ 
+if(personalName !--null && personalName !-- ''){
+    alert(`Hello ${personName} Would you like to learn some python today?`)
+}
+else{
+    alert('you have to fill your name !')
+}
